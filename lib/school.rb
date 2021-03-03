cad4f91492e1b307b5e631d38bcd46d81a11f722
Andrés Nuñez <andresanunez98@gmail.com>
2021-03-03 13:07:46 -0500

    commit tree 9ae91e687f9e732919071943b111b5b65984190c

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
+require 'pry'
 # code here!
+class School
+attr_accessor :name, :roster
+# attr_accessor :student, :grade
+
+def initialize(name, roster = {})
+@name = name
+@roster = roster
+# binding.pry
+end
+
+def add_student(student,grade)
+
+@roster[grade] ||= []
+@roster[grade] << student
+end
+
+def grade(grade)
+ 
+    @roster[grade]
+end
+
+def sort
+    sorted_students = {}
+
+    @roster.each do |grade, students|
+        sorted_students[grade] = students.sort
+    end
+    sorted_students
+end
+
+
+end
